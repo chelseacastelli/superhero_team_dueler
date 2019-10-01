87d4275c2fb43c1f34af38d0b052c8d1cc3b7425
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-30 18:14:43 -0700

    Complete Team class and pass all tests

--- superheroes.py
+++ superheroes.py
@@ -113,28 +113,17 @@ class Team(Hero):
         self.heroes = list()
 
     def remove_hero(self, name):
-         '''Remove hero from heroes list.
-        If Hero isn't found return 0.
-        '''
-        if name not in self.heroes:
-            return 0
+        for hero in self.heroes:
+            if name == hero.name:
+                self.heroes.remove(hero)
 
-        self.heroes.remove(name)
+        return 0
 
+    def view_all_heroes(self):
+        '''Prints out all heroes to the console.'''
+        for hero in self.heroes:
+            print(hero.name)
 
-# if __name__ == "__main__":
-#     hero1 = Hero("Wonder Woman", 200)
-#     hero2 = Hero("Dumbledore", 200)
-#     shield = Armor("Shield", 50)
-#     suit = Armor("Suit", 30)
-#     hero1.add_armor(suit)
-#     hero2.add_armor(shield)
-#     ability1 = Ability("Super Speed", 300)
-#     ability2 = Ability("Super Eyes", 130)
-#     ability3 = Ability("Wizard Wand", 80)
-#     ability4 = Ability("Wizard Beard", 20)
-#     hero1.add_ability(ability1)
-#     hero1.add_ability(ability2)
-#     hero2.add_ability(ability3)
-#     hero2.add_ability(ability4)
-#     hero1.fight(hero2)
+    def add_hero(self, hero):
+        '''Add Hero object to self.heroes.'''
+        self.heroes.append(hero)
